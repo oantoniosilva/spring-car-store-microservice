50e6d10fd00f76bbb3b79821d226a9b54bed186d
Antonio <antoniopsilva083@gmail.com>
2023-06-12 01:51:57 -0300

    chore: initial project setup

---
+++ src/main/java/com/carshop/springmicroservicestore/SpringMicroserviceStoreApplication.java
@@ -0,0 +1,13 @@
+package com.carshop.springmicroservicestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroserviceStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroserviceStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carshop/springmicroservicestore/SpringMicroserviceStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carshop.springmicroservicestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroserviceStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
